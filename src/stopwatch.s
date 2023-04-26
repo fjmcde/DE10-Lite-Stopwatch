fa81574d4bdb308f8ec8abec4d8e527bce5eec07
frmc3658 <frmc3658@colorado.edu>
2023-04-26 09:28:11 -0600

    condensed timer_ISR and started encode_sseg function

--- src/stopwatch.s
+++ src/stopwatch.s
@@ -72,7 +72,11 @@ program_halt:
 
 
 /*************************************************************
- * Interval Timer Init(timer_addr, start_val)
+ * intTimerInit(timer_addr, start_val)
+ *
+ * Details:
+ * Initializes, starts, and enables interrupts for 
+ * desired interval timer
  *
  * param: r4 - timer_addr
  *      Base address for either interval 
@@ -84,7 +88,7 @@ program_halt:
  *      r16: for loading/storing of peripheral registers
  *      r17: for intermediate storage or values
  *
- * return: r2 - validTimer
+ * return: r2 - error_code
  *       Returns 0 for a valid timer address
  *       Returns -1 for an invalid timer address
  ************************************************************/
@@ -156,10 +160,45 @@ end_init:
     ret
 
 
+/*************************************************************
+ * encode_sseg(void)
+ * 
+ * Details:
+ *      Encodes timer value and outputs to the seven segment
+ *      displays.
+ *
+ * param: r4 - timer address
+ *      Address of the timer to encode
+ * param: r5 - count
+ *      Data variable count1 or count 2 for interval timer1
+ *      or timer2, respectively
+ *
+ *  Registers Used:
+ *      r16: for loading/storing of peripheral registers
+ *      r17: for intermediate storage or values
+ *
+ * return: NONE
+ ************************************************************/
 .global encode_sseg
 encode_sseg:
+    # stack prologue
+    subi    sp, sp, 8
+    stw     r16, 0(sp)
+    stw     r17, 4(sp)
 
-	ret
+    # get count value
+    movia    r16, %hi(count)
+
+    # encode count value to Hex displays
+    movia   r17, SSEG_HEX5_4_BASE_ADDR
+    
+
+    # stack epilogue
+    ldw     r17, 4(sp)
+    ldw     r16, 0(sp)
+    addi    sp, sp, 8
+
+    ret
 
 
 /**************************************************
@@ -194,29 +233,37 @@ ISR_External:
     stw     ea, 0(sp)
     stw     r8, 4(sp)
 
-Timer1_ISR:
-    # check if Interval Timer1 IRQ
+Timer_ISR:
+    # check for timer1 IRQ
     andi    r8, et, INT_TIMER1_IRQ
-    beq     r8, r0, Timer2_ISR
+    beq     r8, r0, Timer2
 
-    # store return address on stack
-    subi    sp, sp, 4
-    stw     ra, 0(sp)
+Timer1:
+    # fetch timer1 status register
+    movia   et, INT_TIMER1_BASE_ADDR
+    br      lower_timer_IF
 
-    call    encode_sseg
-
-    # load ra from stack
-    ldw     ra, 0(sp)
-    addi    sp, sp, 4
-
-    br      END_ISR
-
-
-Timer2_ISR:
+Timer2:
+    # check for timer2 IRQ
     andi    r8, et, INT_TIMER2_IRQ
     beq     r8, r0, END_ISR
 
+    # fetch timer2 status register
+    movia   et, INT_TIMER2_BASE_ADDR
+
+lower_timer_IF:
+    # write any value to timer status register to
+    # deasset TO bit; lowing the IF
+    movi    r8, 1
+    stwio   r8, 0(et)
+
+    # increment count
+    movi    r8, count
+    addi    r8, r8, 1
+    stw     r8, 0(count)
+
     # store return address on stack
+    # prior to function call
     subi    sp, sp, 4
     stw     ra, 0(sp)
 
@@ -234,5 +281,12 @@ END_ISR:
     addi    sp, sp, 8
 
     eret
+
+.end
+
+
+.section .data
+count:
+    .word   0
 
 .end
